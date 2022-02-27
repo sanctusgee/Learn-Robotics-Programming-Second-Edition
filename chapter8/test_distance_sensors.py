7e94644cdd38d434503ae857f0dd8b63776d0810
Godwin Effiong <sanctusgee@users.noreply.github.com>
2022-02-27 04:29:54 -0700

    Add pin_factory - fix error of PWMSoftwareFallback
    # Note:  By adding the pin_factory, the changes resolve the error from original code: "PWMSoftwareFallback: For more accurate readings, use the pigpio pin factory."
       
    # References are:
    # -- https://gpiozero.readthedocs.io/en/stable/api_pins.html#changing-the-pin-factory
    # -- https://gpiozero.readthedocs.io/en/stable/api_pins.html#module-gpiozero.pins.pigpio

--- chapter8/test_distance_sensors.py
+++ chapter8/test_distance_sensors.py
@@ -1,12 +1,16 @@
 import time
 from gpiozero import DistanceSensor
+from gpiozero.pins.pigpio import PiGPIOFactory
+factory = PiGPIOFactory()
 
-print("Prepare GPIO Pins")
-sensor_l = DistanceSensor(echo=17, trigger=27, queue_len=2)
-sensor_r = DistanceSensor(echo=5,  trigger=6, queue_len=2)
+print("Prepare GPIO pins")
+# register the sensors and map to Raspberry Pi pins
+sensor_left = DistanceSensor(echo=17, trigger=27, queue_len=2, pin_factory=factory)
+sensor_right = DistanceSensor(echo=5, trigger=6, queue_len=2, pin_factory=factory)
 
+# print out distance to sensor
 while True:
-    print("Left: {l:.2f}, Right: {r:.2f}".format(
-        l=sensor_l.distance * 100, 
-        r=sensor_r.distance * 100))
+    print(f'Left: {sensor_left.distance * 100:.3f}, Right: {sensor_right.distance * 100:.3f}')
+    # force pause to avoid flooding output
     time.sleep(0.1)
+
